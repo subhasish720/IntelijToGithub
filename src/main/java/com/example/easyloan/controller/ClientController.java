148fed4aee5c87976ef83451a79391a027a90d49
subhasish <subhasishpadhy55@gmail.com>
2026-02-07 02:14:12 +0530

    full project

---
+++ src/main/java/com/example/easyloan/controller/ClientController.java
@@ -0,0 +1,12 @@
+package com.example.easyloan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController {
+    @GetMapping("/api/get/details")
+    public void clientDetails(){
+
+    }
+}
